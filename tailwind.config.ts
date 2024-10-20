ad00e385fcb06df15b166fbb73c0da5885ef9016
Klymenko Nikita <146847436+PolterPZ@users.noreply.github.com>
2024-10-20 23:14:53 +0300

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  darkMode: "class",
+
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  perspective: {
+    "1500": "1500px",
+  },
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "my-bg": "#111",
+      },
+      keyframes: {
+        vibrate: {
+          "0%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(5deg)" },
+          "50%": { transform: "rotate(-5deg)" },
+          "75%": { transform: "rotate(5deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        vibrate: "vibrate 0.3s ease-in-out infinite", // Define animation
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
